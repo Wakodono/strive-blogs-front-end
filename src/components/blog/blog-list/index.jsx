0cf972aeb56ef747b5276ed0e3826fca4931fddf
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-10-24 22:19:39 +0100

    Includes Blogs fetch

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -2,13 +2,30 @@ import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
 import posts from "../../../data/posts.json";
+
+const url = "http://localhost:3001/blogs"
 export default class BlogList extends Component {
+
+  state = {
+    blogs: []
+  }
+
+  fetchBlogs = async () => {
+  const  blogs = await fetch(url)
+  const response = await blogs.json()
+  this.setState({response})
+  }
+
+  componentDidMount() {
+    this.fetchBlogs()
+  }
+
   render() {
     return (
       <Row>
-        {posts.map((post) => (
+        {this.state.blogs.map((blog) => (
           <Col md={4} style={{ marginBottom: 50 }}>
-            <BlogItem key={post.title} {...post} />
+            <BlogItem key={blog.title} {...blog} />
           </Col>
         ))}
       </Row>
